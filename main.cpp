8b09d08a31f19ce9c01f639f4a1909166b20fbe4
ketzon <bessonflorian42@yahoo.fr>
2024-07-12 15:43:23 +0200

    clean

--- main.cpp
+++ main.cpp
@@ -1,22 +1,15 @@
 #include "irc.hpp"
 
-int main(int ac, char **av) {
-    if (ac != 3) {
-        std::cerr << "./ircserv <port> <password>\n";
-        return 1;
-    }
-
+int main(int ac, char **av) 
+{
+    if (ac != 3) 
+        return std::cerr << "./ircserv <port> <password>\n", 1;
     int port = atoi(av[1]);
-    if (port <= 0 || port > 65535) {
-        std::cerr << "Invalid port number" << std::endl;
-        return 1;
-    }
-
     std::string password = av[2];
-
+    if (port <= 0 || port > 65535)
+        return std::cerr << "Invalid port number" << std::endl, 1;
     IrcServer server(port, password);
     server.run();
-
     return 0;
 }
 
