12a04e50ce4a3c6c13a43bb1b570d2610a17c6ba
ketzon <bessonflorian42@yahoo.fr>
2024-07-13 13:57:50 +0200

    test add join

--- irc.cpp
+++ irc.cpp
@@ -1,10 +1,11 @@
 #include "irc.hpp"
+#include <sstream>
+#include <algorithm>
 
 IrcServer::IrcServer(int p, const std::string &pw) : _port(p), _password(pw), _server_fd(-1) 
 {
 }
 
-//setup telephone
 void IrcServer::setupServerSocket() 
 {
     _server_fd = socket(AF_INET, SOCK_STREAM, 0);
@@ -14,36 +15,31 @@ void IrcServer::setupServerSocket()
         exit(EXIT_FAILURE);
     }
 
-    //configure ligne telephonique
     sockaddr_in address;
     address.sin_family = AF_INET;
     address.sin_port = htons(this->_port);
     address.sin_addr.s_addr = INADDR_ANY;
     int opt = 1;
 
-    //permet de reutiliser le telephone rapidement apres deco(socket)
     if (setsockopt(_server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) 
     {
         perror("setsockopt failed");
         exit(EXIT_FAILURE);
     }
 
-    //connecte le telephone a la ligne telephonique (adresse et port)
     if (bind(_server_fd, (struct sockaddr *)&address, sizeof(address))) 
     {
         perror("bind failed");
         exit(EXIT_FAILURE);
     }
 
-    //pret a recevoir des appel, manque plus que le config
     if (listen(_server_fd, 10) < 0) 
     {
         perror("listen failed");
         exit(EXIT_FAILURE);
     }
 
-    //configure telephone appel entrant
-    pollfd server_pollfd;
+    struct pollfd server_pollfd;
     server_pollfd.fd = _server_fd;
     server_pollfd.events = POLLIN;
     _fds.push_back(server_pollfd);
@@ -54,17 +50,15 @@ void IrcServer::run()
     setupServerSocket();
     std::cout << "Server running on port " << _port << " with password " << _password << std::endl;
 
-    //garde le telephone en mode attente pour recevoir des events (call)
     while (true) 
     {
-        int poll_count = poll(_fds.data(), _fds.size(), -1);
+        int poll_count = poll(&_fds[0], _fds.size(), -1);
         if (poll_count < 0) 
         {
             perror("poll failed");
             exit(EXIT_FAILURE);
         }
 
-        //gere les event dentre pollin (gere les call client)
         for (size_t i = 0; i < _fds.size(); ++i) 
         {
             if (_fds[i].revents & POLLIN) 
@@ -82,7 +76,6 @@ void IrcServer::run()
     }
 }
 
-//repond a un appel telephonique
 void IrcServer::acceptNewClient() 
 {
     sockaddr_in client_address;
@@ -94,15 +87,17 @@ void IrcServer::acceptNewClient()
         return;
     }
 
-    pollfd client_pollfd;
+    struct pollfd client_pollfd;
     client_pollfd.fd = client_fd;
     client_pollfd.events = POLLIN;
     _fds.push_back(client_pollfd);
 
+    User* new_user = new User(client_fd);
+    _users[client_fd] = new_user;
+
     std::cout << "New client connected" << std::endl;
 }
 
-//ecoute le message d'un client
 void IrcServer::handleClientMessage(int client_fd) 
 {
     char buffer[1024];
@@ -117,24 +112,72 @@ void IrcServer::handleClientMessage(int client_fd)
         {
             perror("read failed");
         }
-        close(client_fd);
-
-        // Remove from poll vector
-        for (size_t i = 0; i < _fds.size(); ++i) 
-        {
-            if (_fds[i].fd == client_fd) 
-            {
-                _fds.erase(_fds.begin() + i);
-                break;
-            }
-        }
+        removeClient(client_fd);
     } 
     else 
     {
         buffer[bytes_read] = '\0';
-        std::cout << "Received message: " << buffer << std::endl;
-        // Echo message back to client
-        write(client_fd, buffer, bytes_read);
+        std::string command(buffer);
+        handleCommand(client_fd, command);
+    }
+}
+
+void IrcServer::handleCommand(int client_fd, const std::string &command) 
+{
+    std::istringstream iss(command);
+    std::string cmd;
+    iss >> cmd;
+
+    if (cmd == "NICK") 
+    {
+        std::string nick;
+        iss >> nick;
+        _users[client_fd]->nickname = nick;
+        std::string response = "Your nickname is now " + nick + "\n";
+        write(client_fd, response.c_str(), response.size());
+    } 
+    else if (cmd == "JOIN") 
+    {
+        std::string channel_name;
+        iss >> channel_name;
+
+        if (_channels.find(channel_name) == _channels.end()) 
+        {
+            _channels[channel_name] = Channel(channel_name);
+        }
+
+        _channels[channel_name].users.push_back(_users[client_fd]);
+        std::string response = "You joined channel " + channel_name + "\n";
+        write(client_fd, response.c_str(), response.size());
+    } 
+    else 
+    {
+        std::string response = "Unknown command\n";
+        write(client_fd, response.c_str(), response.size());
+    }
+}
+
+void IrcServer::removeClient(int client_fd) 
+{
+    close(client_fd);
+
+    for (size_t i = 0; i < _fds.size(); ++i) 
+    {
+        if (_fds[i].fd == client_fd) 
+        {
+            _fds.erase(_fds.begin() + i);
+            break;
+        }
+    }
+
+    User* user = _users[client_fd];
+    _users.erase(client_fd);
+    delete user;
+
+    for (std::map<std::string, Channel>::iterator it = _channels.begin(); it != _channels.end(); ++it) 
+    {
+        Channel& channel = it->second;
+        channel.users.erase(std::remove(channel.users.begin(), channel.users.end(), user), channel.users.end());
     }
 }
 
--- irc.hpp
+++ irc.hpp
@@ -1,18 +1,40 @@
 #pragma once
 
 #include <iostream>
-#include <cstdio>
 #include <cstdlib>
-#include <vector>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <netinet/in.h>
+#include <cstdio>
+#include <cstring>
 #include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 #include <arpa/inet.h>
-#include <poll.h>
-#include <csignal>
+#include <vector>
+#include <map>
+#include <string>
+#include <poll.h>  
 
-class IrcServer {
+class User 
+{
+public:
+    int fd;
+    std::string nickname;
+    std::string username;
+
+    User(int fd) : fd(fd), nickname(""), username("") {}
+};
+
+class Channel 
+{
+public:
+    std::string name;
+    std::vector<User*> users;
+
+    Channel() {}
+    Channel(const std::string& name) : name(name) {}
+};
+
+class IrcServer 
+{
 public:
     IrcServer(int p, const std::string &pw);
     void run(void);
@@ -21,10 +43,14 @@ private:
     int _port;
     std::string _password;
     int _server_fd;
-    std::vector<pollfd> _fds;
+    std::vector<struct pollfd> _fds;
+    std::map<int, User*> _users;
+    std::map<std::string, Channel> _channels;
 
     void setupServerSocket();
     void acceptNewClient();
     void handleClientMessage(int client_fd);
+    void handleCommand(int client_fd, const std::string &command);
+    void removeClient(int client_fd);
 };
 
